932ad858ae7457bbf82fc38c6faa26adc9875896
“Edan <2777195h@student.gla.ac.uk>
2024-01-22 15:36:25 +0000

    chapter 4

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(requesst):
-    return HttpResponse("Rango says hey there partner!")
+def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'your_name': 'Edan Hynes'}  # Replace with your name
+    return render(request,'rango/about.html',context=context_dict)
 
